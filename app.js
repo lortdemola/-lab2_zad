0aaa17fb84f870d151e93c51eb63e80a4c0e300a
lortdemola i <97162568+lortdemola@users.noreply.github.com>
2022-10-10 20:09:22 +0000

    final push

--- app.js
+++ app.js
@@ -15,5 +15,25 @@ module.exports = class app {
   MIN() {
     console.log( _.min(this.newtab));
   }
-  
+   USER(app){
+    let avg = 0;
+    let avg_temp = 0;
+        for(let i of app.allGrades){
+          for(let x of i.grades){
+            avg += x * i.weigth
+            avg_temp+=i.weigth
+          }
+          
+        }
+      avg = _.round(avg/avg_temp,2)
+     console.log(app.name +' '+app.surname +' '+ avg)
+  }
+  FIND(app){
+  console.log(_.find(app.allGrades, function(o) {  if(o.weigth == 1){return o.subjectName}}))
+  }
+  GETmail(cols){
+    const reg = /^[-\w\.]+@([-\w]+\.)+[a-z]+$/i;
+    console.log(_.filter(cols, item => reg.test(item)).sort());
+  }
+ 
 }
--- index.js
+++ index.js
@@ -1,8 +1,46 @@
 const _ = require('lodash');
 const app = require('./app.js');
 
+const user ={
+  name:'imie',
+  surname:'nazwisko',
+  allGrades: [
+    {
+      subjectName:'Name1',
+      grades: [5,4,3,5,2],
+      weigth: 3
+    },
+    {
+      subjectName:'Name2',
+      grades: [3,3.5,2],
+      weigth: 1
+    },
+    {
+      subjectName:'Name3',
+      grades: [4,3,3.5],
+      weigth: 5
+    }
+  ]
+} 
+ 
+const collections = [
+{},
+15,
+"hello@test.pl",
+null,
+['aaa', 'bbb', 5],
+'admin@gmail.com',
+undefined,
+'a34@yahoo.com',
+'321@a',
+'321.pl'
+];
+
 const zad1 = new app([9,1,2,3,4,5])
 zad1.AVG();
 zad1.MAX();
 zad1.MIN();
+zad1.USER(user);
+zad1.FIND(user);
+zad1.GETmail(collections);
 
